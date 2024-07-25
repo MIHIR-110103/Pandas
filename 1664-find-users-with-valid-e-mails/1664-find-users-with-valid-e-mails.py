51105da9d16d3ab2b79549ed37e45d4ecbdf8a81
Mihir Randive <108995481+MIHIR-110103@users.noreply.github.com>
2024-07-25 12:21:42 +0530

    Time: 327 ms (99.7%), Space: 70.4 MB (7.16%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
-    valid = users[users["mail"].str.match(r'^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\?com)?\.com$')]
+    valid = users[users["mail"].str.contains(r'^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\?com)?\.com$')]
 
     return valid
